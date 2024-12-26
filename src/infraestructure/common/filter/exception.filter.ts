9458ef934591eaf71a94d183231ce194270bfbf4
Javier Flores <jffc.dev@gmail.com>
2024-12-25 23:49:50 -0500

    feat: UnifiedExceptionFilter handler added

---
+++ src/infraestructure/common/filter/exception.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch()
+export class UnifiedExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    console.log('javier', exception);
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+    const message = exception.message || 'Internal Server Error';
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      message,
+      errorCode: exception.code || 'UNEXPECTED_ERROR',
+    });
+  }
+}
