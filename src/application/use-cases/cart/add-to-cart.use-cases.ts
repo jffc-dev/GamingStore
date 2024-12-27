b001b0c80799a9941055e9098400f3e3c8573555
Javier Flores <jffc.dev@gmail.com>
2024-12-27 10:14:11 -0500

    feat: AddProductToCartUseCase added

---
+++ src/application/use-cases/cart/add-to-cart.use-cases.ts
@@ -0,0 +1,39 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CartDetailRepository } from 'src/application/contracts/persistence/cart.repository';
+import { ProductRepository } from 'src/application/contracts/persistence/product.repository';
+import { CartDetail } from 'src/domain/cart-detail';
+
+interface IAddProductToCartUseCaseProps {
+  productId: string;
+  userId: string;
+  quantity: number;
+}
+@Injectable()
+export class AddProductToCartUseCase {
+  constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly cartDetailRepository: CartDetailRepository,
+  ) {}
+
+  async execute({
+    productId,
+    quantity,
+    userId,
+  }: IAddProductToCartUseCaseProps): Promise<any> {
+    const productResponse =
+      await this.productRepository.getProductById(productId);
+
+    if (productResponse.stock < quantity) {
+      throw new BadRequestException('Insufficient stock');
+    }
+
+    const cartDetail = new CartDetail({
+      productId,
+      userId,
+      quantity,
+    });
+
+    const cartDetailResponse = this.cartDetailRepository.addToCart(cartDetail);
+    return cartDetailResponse;
+  }
+}
