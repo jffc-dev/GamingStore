b17c1d93449205a1ef6181abc1922ee670e68936
Javier Flores <jffc.dev@gmail.com>
2025-01-02 14:29:55 -0500

    fix: order and auth modules refatored

--- src/infraestructure/graphql/resolvers/order/order.module.ts
+++ src/infraestructure/graphql/resolvers/order/order.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { OrderResolver } from './order.resolver';
 import { CreateOrderFromCartUseCase } from 'src/application/use-cases/order/order-from-cart.use-case';
 import { UuidModule } from 'src/infraestructure/services/uuid/uuid.module';
-import { GetUserOrdersUseCase } from 'src/application/use-cases/order/get-user-orders.use-case';
 import { OrderDetailsLoader } from './dataloaders/order-details.loader';
 import { GetOrderDetailsUseCase } from 'src/application/use-cases/order/get-order-details.use-case';
+import { GetUserOrdersUseCase } from 'src/application/use-cases/order/get-orders.use-case';
 
 @Module({
   providers: [
     CreateOrderFromCartUseCase,
-    GetUserOrdersUseCase,
     GetOrderDetailsUseCase,
+    GetUserOrdersUseCase,
 
     OrderResolver,
 
